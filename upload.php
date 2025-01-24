d4e72ea87b127094e71e78ea5167366e69b7ca7e
tejasKhamkar2211 <tejaskhamkar23@gmail.com>
2025-01-24 19:57:07 +0530

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,30 @@
+<?php
+$fname=$_POST["fname"];
+$lname=$_POST["lname"];
+echo $fname;
+echo $lname;
+
+
+
+$servername = "dbpod";
+$username = "root";
+$password = "Satara@123";
+$dbname = "facebook";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO users (fname, lname) VALUES ('$fname','$lname')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
